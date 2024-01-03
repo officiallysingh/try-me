6ab1c0835eeb9a3c129d06f26860984bf17889e0
officiallysingh <raj14.1984@gmail.com>
2024-01-03 09:14:49 +0530

    updated

--- src/main/java/com/ksoot/TryMe.java
+++ src/main/java/com/ksoot/TryMe.java
@@ -1,9 +1,19 @@
 package com.ksoot;
 
-import java.time.OffsetDateTime;
+import net.datafaker.Faker;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.*;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Random;
 
 public class TryMe {
+
     public static void main(String[] args) {
-        System.out.println(OffsetDateTime.now());
+        Long version = null;
+        System.out.println(version == 0);
     }
 }
